061b0a5ff2810eecb45e7a5a11bfadaea8d230ad
Lakshmi Priya Sekar <lasekar@microsoft.com>
2017-12-01 16:43:26 -0800

    Respond to PR feedback.

---
+++ src/Common/src/Interop/Unix/System.Security.Cryptography.Native/Interop.OpenSslVersion.cs
@@ -0,0 +1,49 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Net.Security;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Authentication;
+using System.Security.Authentication.ExtendedProtection;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Win32.SafeHandles;
+
+internal static partial class Interop
+{
+    internal static partial class OpenSsl
+    {
+        private static Version s_opensslVersion;
+
+        [DllImport(Libraries.CryptoNative, EntryPoint = "CryptoNative_SSLEayVersion")]
+        private static extern string OpenSslVersionDescription();
+
+        private static Version OpenSslVersion
+        {
+            get
+            {
+                if (s_opensslVersion == null)
+                {
+                    const string OpenSSL = "OpenSSL ";
+
+                    // Skip OpenSSL part, and get the version string of format x.y.z
+                    if (!Version.TryParse(OpenSslVersionDescription().Substring(OpenSSL.Length, 5), out s_opensslVersion))
+                    {
+                        s_opensslVersion = new Version(0, 0, 0);
+                    }
+                }
+
+                return s_opensslVersion;
+            }
+        }
+    }
+}
