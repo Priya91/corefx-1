84e73cb9ee56f6357a316ba66f8b2a3b9c2f3682
Lakshmi Priya Sekar <lasekar@microsoft.com>
2018-02-02 13:46:33 -0800

    Add test infra for auth testing.

--- src/Common/tests/System/Net/Http/LoopbackServer.cs
+++ src/Common/tests/System/Net/Http/LoopbackServer.cs
@@ -22,6 +22,13 @@ namespace System.Net.Test.Common
     {
         public static Func<HttpRequestMessage, X509Certificate2, X509Chain, SslPolicyErrors, bool> AllowAllCertificates = (_, __, ___, ____) => true;
 
+        private enum AuthenticationProtocols
+        {
+            Basic,
+            Digest,
+            None
+        }
+
         public class Options
         {
             public IPAddress Address { get; set; } = IPAddress.Loopback;
@@ -30,6 +37,9 @@ namespace System.Net.Test.Common
             public SslProtocols SslProtocols { get; set; } = SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12;
             public bool WebSocketEndpoint { get; set; } = false;
             public Func<Stream, Stream> ResponseStreamWrapper { get; set; }
+            public string Domain { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
         }
 
         public static Task CreateServerAsync(Func<Socket, Uri, Task> funcAsync, Options options = null)
@@ -49,7 +59,7 @@ namespace System.Net.Test.Common
                 server.Listen(options.ListenBacklog);
 
                 localEndPoint = (IPEndPoint)server.LocalEndPoint;
-                string host = options.Address.AddressFamily == AddressFamily.InterNetworkV6 ? 
+                string host = options.Address.AddressFamily == AddressFamily.InterNetworkV6 ?
                     $"[{localEndPoint.Address}]" :
                     localEndPoint.Address.ToString();
 
@@ -100,6 +110,11 @@ namespace System.Net.Test.Common
             return AcceptSocketAsync(server, (s, stream, reader, writer) => ReadWriteAcceptedAsync(s, reader, writer, response), options);
         }
 
+        public static Task<List<string>> ReadRequestAndAuthenticateAsync(Socket server, string response, Options options)
+        {
+            return AcceptSocketAsync(server, (s, stream, reader, writer) => ValidateAuthenticationAsync(s, reader, writer, response, options), options);
+        }
+
         public static async Task<List<string>> ReadWriteAcceptedAsync(Socket s, StreamReader reader, StreamWriter writer, string response = null)
         {
             // Read request line and headers. Skip any request body.
@@ -113,6 +128,247 @@ namespace System.Net.Test.Common
             await writer.WriteAsync(response ?? DefaultHttpResponse).ConfigureAwait(false);
 
             return lines;
+        }
+
+        public static async Task<List<string>> ValidateAuthenticationAsync(Socket s, StreamReader reader, StreamWriter writer, string response, Options options)
+        {
+            // Send unauthorized response from server.
+            await ReadWriteAcceptedAsync(s, reader, writer, response);
+
+            // Read the request method.
+            string line = await reader.ReadLineAsync().ConfigureAwait(false);
+            int index = line != null ? line.IndexOf(' ') : -1;
+            string requestMethod = null;
+            if (index != -1)
+            {
+                requestMethod = line.Substring(0, index);
+            }
+
+            // Read the authorization header from client.
+            AuthenticationProtocols protocol = AuthenticationProtocols.None;
+            string clientResponse = null;
+            while (!string.IsNullOrEmpty(line = await reader.ReadLineAsync().ConfigureAwait(false)))
+            {
+                if (line.StartsWith("Authorization"))
+                {
+                    clientResponse = line;
+                    if (line.Contains(nameof(AuthenticationProtocols.Basic)))
+                    {
+                        protocol = AuthenticationProtocols.Basic;
+                        break;
+                    }
+                    else if (line.Contains(nameof(AuthenticationProtocols.Digest)))
+                    {
+                        protocol = AuthenticationProtocols.Digest;
+                        break;
+                    }
+                }
+            }
+
+            bool success = false;
+            switch (protocol)
+            {
+                case AuthenticationProtocols.Basic:
+                    success = IsBasicAuthTokenValid(line, options);
+                    break;
+
+                case AuthenticationProtocols.Digest:
+                    // Read the request content.
+                    string requestContent = null;
+                    while (!string.IsNullOrEmpty(line = await reader.ReadLineAsync().ConfigureAwait(false)))
+                    {
+                        if (line.Contains("Content-Length"))
+                        {
+                            line = await reader.ReadLineAsync().ConfigureAwait(false);
+                            while (!string.IsNullOrEmpty(line = await reader.ReadLineAsync().ConfigureAwait(false)))
+                            {
+                                requestContent += line;
+                            }
+                        }
+                    }
+
+                    success = IsDigestAuthTokenValid(clientResponse, requestContent, requestMethod, options);
+                    break;
+            }
+
+            if (success)
+            {
+                await writer.WriteAsync(DefaultHttpResponse).ConfigureAwait(false);
+            }
+            else
+            {
+                await writer.WriteAsync(response).ConfigureAwait(false);
+            }
+
+            return null;
+        }
+
+        private static bool IsBasicAuthTokenValid(string clientResponse, Options options)
+        {
+            string clientHash = clientResponse.Substring(clientResponse.IndexOf(nameof(AuthenticationProtocols.Basic), StringComparison.OrdinalIgnoreCase) +
+                nameof(AuthenticationProtocols.Basic).Length).Trim();
+            string userPass = string.IsNullOrEmpty(options.Domain) ? options.Username + ":" + options.Password : options.Domain + "\\" + options.Username + ":" + options.Password;
+            return clientHash == Convert.ToBase64String(Encoding.UTF8.GetBytes(userPass));
+        }
+
+        private static bool IsDigestAuthTokenValid(string clientResponse, string requestContent, string requestMethod, Options options)
+        {
+            string clientHash = clientResponse.Substring(clientResponse.IndexOf(nameof(AuthenticationProtocols.Digest), StringComparison.OrdinalIgnoreCase) +
+                nameof(AuthenticationProtocols.Digest).Length).Trim();
+            string[] values = clientHash.Split(',');
+
+            string username = null, uri = null, realm = null, nonce = null, response = null, algorithm = null, cnonce = null, opaque = null, qop = null, nc = null;
+            bool userhash = false;
+            for (int i = 0; i < values.Length; i++)
+            {
+                string trimmedValue = values[i].Trim();
+                if (trimmedValue.Contains(nameof(username)))
+                {
+                    // Username is a quoted string.
+                    int startIndex = trimmedValue.IndexOf('"') + 1;
+
+                    if (startIndex != -1)
+                        username = trimmedValue.Substring(startIndex, trimmedValue.Length - startIndex - 1);
+
+                    // Username is mandatory.
+                    if (string.IsNullOrEmpty(username))
+                        return false;
+                }
+                if (trimmedValue.Contains(nameof(userhash)) && trimmedValue.Contains("true"))
+                {
+                    userhash = true;
+                }
+                else if (trimmedValue.Contains(nameof(uri)))
+                {
+                    int startIndex = trimmedValue.IndexOf('"') + 1;
+                    if (startIndex != -1)
+                        uri = trimmedValue.Substring(startIndex, trimmedValue.Length - startIndex - 1);
+
+                    // Request uri is mandatory.
+                    if (string.IsNullOrEmpty(uri))
+                        return false;
+                }
+                else if (trimmedValue.Contains(nameof(realm)))
+                {
+                    // Realm is a quoted string.
+                    int startIndex = trimmedValue.IndexOf('"') + 1;
+                    if (startIndex != -1)
+                        realm = trimmedValue.Substring(startIndex, trimmedValue.Length - startIndex - 1);
+
+                    // Realm is mandatory.
+                    if (string.IsNullOrEmpty(realm))
+                        return false;
+                }
+                else if (trimmedValue.Contains(nameof(cnonce)))
+                {
+                    // CNonce is a quoted string.
+                    int startIndex = trimmedValue.IndexOf('"') + 1;
+                    if (startIndex != -1)
+                        cnonce = trimmedValue.Substring(startIndex, trimmedValue.Length - startIndex - 1);
+                }
+                else if (trimmedValue.Contains(nameof(nonce)))
+                {
+                    // Nonce is a quoted string.
+                    int startIndex = trimmedValue.IndexOf('"') + 1;
+                    if (startIndex != -1)
+                        nonce = trimmedValue.Substring(startIndex, trimmedValue.Length - startIndex - 1);
+
+                    // Nonce is mandatory.
+                    if (string.IsNullOrEmpty(nonce))
+                        return false;
+                }
+                else if (trimmedValue.Contains(nameof(response)))
+                {
+                    // response is a quoted string.
+                    int startIndex = trimmedValue.IndexOf('"') + 1;
+                    if (startIndex != -1)
+                        response = trimmedValue.Substring(startIndex, trimmedValue.Length - startIndex - 1);
+
+                    // Response is mandatory.
+                    if (string.IsNullOrEmpty(response))
+                        return false;
+                }
+                else if (trimmedValue.Contains(nameof(algorithm)))
+                {
+                    int startIndex = trimmedValue.IndexOf('=') + 1;
+                    if (startIndex != -1)
+                        algorithm = trimmedValue.Substring(startIndex, trimmedValue.Length - startIndex).Trim();
+
+                    if (string.IsNullOrEmpty(algorithm))
+                        algorithm = "sha-256";
+                }
+                else if (trimmedValue.Contains(nameof(opaque)))
+                {
+                    // Opaque is a quoted string.
+                    int startIndex = trimmedValue.IndexOf('"') + 1;
+                    if (startIndex != -1)
+                        opaque = trimmedValue.Substring(startIndex, trimmedValue.Length - startIndex - 1);
+                }
+                else if (trimmedValue.Contains(nameof(qop)))
+                {
+                    int startIndex = trimmedValue.IndexOf('=') + 1;
+                    if (startIndex != -1)
+                        qop = trimmedValue.Substring(startIndex, trimmedValue.Length - startIndex).Trim();
+                }
+                else if (trimmedValue.Contains(nameof(nc)))
+                {
+                    int startIndex = trimmedValue.IndexOf('=') + 1;
+                    if (startIndex != -1)
+                        nc = trimmedValue.Substring(startIndex, trimmedValue.Length - startIndex).Trim();
+                }
+            }
+
+            // Verify username.
+            if (userhash && ComputeHash(options.Username + ":" + realm, algorithm) != username)
+            {
+                return false;
+            }
+
+            if (!userhash && options.Username != username)
+            {
+                return false;
+            }
+
+            // Calculate response and compare with the client response hash.
+            string a1 = options.Username + ":" + realm + ":" + options.Password;
+            if (algorithm.Contains("sess"))
+            {
+                a1 = ComputeHash(a1, algorithm) + ":" + nonce + ":" + cnonce ?? string.Empty;
+            }
+
+            string a2 = requestMethod + ":" + uri;
+            if (qop.Equals("auth-int"))
+            {
+                string content = requestContent ?? string.Empty;
+                a2 = a2 + ":" + ComputeHash(content, algorithm);
+            }
+
+            string serverResponseHash = ComputeHash(ComputeHash(a1, algorithm) + ":" +
+                                        nonce + ":" +
+                                        nc + ":" +
+                                        cnonce + ":" +
+                                        qop + ":" +
+                                        ComputeHash(a2, algorithm), algorithm);
+
+            return response == serverResponseHash;
+        }
+
+        private static string ComputeHash(string data, string algorithm)
+        {
+            // Disable MD5 insecure warning.
+#pragma warning disable CA5351
+            using (HashAlgorithm hash = algorithm.Contains("SHA-256") ? SHA256.Create() : (HashAlgorithm)MD5.Create())
+#pragma warning restore CA5351
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(data));
+
+                StringBuilder sb = new StringBuilder(result.Length * 2);
+                foreach (byte b in result)
+                    sb.Append(b.ToString("x2"));
+
+                return sb.ToString();
+            }
         }
 
         public static async Task<bool> WebSocketHandshakeAsync(Socket s, StreamReader reader, StreamWriter writer)
@@ -226,7 +482,7 @@ namespace System.Net.Test.Common
             {
                 // Read past request headers.
                 string line;
-                while (!string.IsNullOrEmpty(line = reader.ReadLine())) ;
+                while (!string.IsNullOrEmpty(line = reader.ReadLine()));
 
                 // Determine response transfer headers.
                 string transferHeader = null;
@@ -272,6 +528,6 @@ namespace System.Net.Test.Common
 
                 return null;
             }), out localEndPoint);
-        }        
+        }
     }
 }
